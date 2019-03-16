3b0ace09a641c5242322d755e3126dc4b7fde33a
Victor Potasyev <vpotasyev@gmail.com>
2019-03-16 13:47:21 +0300

    Added sprite to GameObject

--- bubble-mubble/Classes/GameObject.cpp
+++ bubble-mubble/Classes/GameObject.cpp
@@ -2,11 +2,17 @@
 
 
 
-GameObject::GameObject()
+GameObject::GameObject(cocos2d::Sprite* sprite)
+  : sprite_(sprite)
 {
 }
 
 
 GameObject::~GameObject()
 {
+}
+
+cocos2d::Sprite * GameObject::getSprite()
+{
+  return sprite_;
 }
--- bubble-mubble/Classes/GameObject.h
+++ bubble-mubble/Classes/GameObject.h
@@ -1,8 +1,14 @@
 #pragma once
+
+#include "cocos2d.h"
+
 class GameObject
 {
 public:
   GameObject();
   virtual ~GameObject();
+
+protected:
+  cocos2d::Sprite* sprite_;
 };
 
